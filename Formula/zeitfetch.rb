f9fe665afe4460d27124d841a74a91691641a095
nidnogg <henriquevt98@gmail.com>
2022-12-17 00:09:29 -0300

    Fixed SHA-256 hash

--- Formula/zeitfetch.rb
+++ Formula/zeitfetch.rb
@@ -5,7 +5,7 @@ class Zeitfetch < Formula
   desc "Instantaneous snapshots of system information"
   homepage "https://github.com/nidnogg/zeitfetch"
   url "https://github.com/nidnogg/zeitfetch/releases/download/v0.1.4/zeitfetch.tar.gz"
-  sha256 "7344697473c28f0fa98d1fc945a482f553f545d328a62602ffa176ce29a39510"
+  sha256 "8a12e351aff2b735777cf4075f43b79abab54851b70cfa037fd636e7129288af"
   version "0.1.4"
 
   def install
