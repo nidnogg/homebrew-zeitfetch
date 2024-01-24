dd9152941fe257ccfafcf6b1e33779e514cad8ad
nidnogg <henriquevt98@gmail.com>
2024-01-24 19:17:35 -0300

    Bumped version up to v0.1.9

--- Formula/zeitfetch.rb
+++ Formula/zeitfetch.rb
@@ -4,9 +4,9 @@
 class Zeitfetch < Formula
   desc "Instantaneous snapshots of system information"
   homepage "https://github.com/nidnogg/zeitfetch"
-  url "https://github.com/nidnogg/zeitfetch/releases/download/v0.1.8/zeitfetch.tar.gz"
-  sha256 "9fccaffe09dc449b259f04a6915d9e60f79cdead60c090308345d03fffaba4b2"
-  version "0.1.8"
+  url "https://github.com/nidnogg/zeitfetch/releases/download/v0.1.9/zeitfetch.tar.gz"
+  sha256 "593a3075f69517dea72a9378cd3a9776ea2f9c90cdc0de2ee478d38c18fbe956"
+  version "0.1.9"
 
   def install
     bin.install "zeitfetch"
