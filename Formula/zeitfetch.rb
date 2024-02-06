e600698d7c0a68c85e056ac3761e2a6bb0d8ea17
nidnogg <henriquevt98@gmail.com>
2024-02-06 14:51:58 -0300

    Updated metadata retrieval

--- Formula/zeitfetch.rb
+++ Formula/zeitfetch.rb
@@ -2,11 +2,14 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 require "json"
+require 'open-uri'
 class Zeitfetch < Formula
   desc "Instantaneous snapshots of system information"
   homepage "https://github.com/nidnogg/zeitfetch"
   
-  metadata = JSON.parse(File.read("metadata.json"))
+  metadata_json = URI.open("https://raw.githubusercontent.com/nidnogg/homebrew-zeitfetch/main/Formula/metadata.json").read
+  metadata = JSON.parse(metadata_json)
+  
   url "https://github.com/nidnogg/zeitfetch/releases/download/v#{metadata['version']}/zeitfetch.tar.gz"
   sha256 metadata["sha256"]
   version metadata["version"]
