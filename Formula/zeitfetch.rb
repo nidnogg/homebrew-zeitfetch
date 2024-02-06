1fdb537824668a58ef38dbb2857706e1de8834c9
nidnogg <henriquevt98@gmail.com>
2024-02-06 14:01:38 -0300

    Update to version v0.1.10

--- Formula/zeitfetch.rb
+++ Formula/zeitfetch.rb
@@ -1,12 +1,15 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+require "json"
 class Zeitfetch < Formula
   desc "Instantaneous snapshots of system information"
   homepage "https://github.com/nidnogg/zeitfetch"
-  url "https://github.com/nidnogg/zeitfetch/releases/download/v0.1.9/zeitfetch.tar.gz"
-  sha256 "593a3075f69517dea72a9378cd3a9776ea2f9c90cdc0de2ee478d38c18fbe956"
-  version "0.1.9"
+  
+  metadata = JSON.parse("../metadata.json")
+  url "https://github.com/nidnogg/zeitfetch/releases/download/v#{metadata['version']}/zeitfetch.tar.gz"
+  sha256 metadata["sha256"]
+  version metadata["version"]
 
   def install
     bin.install "zeitfetch"
